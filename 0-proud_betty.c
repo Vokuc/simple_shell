c262b4596d362e2a761c49cfe831ee9e94b507b6
Vokuc <ukpaival@gmail.com>
2022-06-14 14:09:21 -0700

    passes betty checks

---
+++ 0-proud_betty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - a beautiful code that passes the Betty checks
+ *Return: 1 always
+ */
+
+int main(void)
+{
+	write(2, "First Shell project", 32);
+	return (1);
+}
